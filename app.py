9712090cbdac2e0651b9d54bb12434fe1dca7a9d
Sithila Herath <sithilaherath1090@gmail.com>
2025-09-22 14:10:57 +0530

    Build server-side loggers

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from helper import preprocessing, vectorizer, get_prediction
 from logger import logging
 
 app = Flask (__name__)
+logging.info('Flask server started')
 
 data = dict()
 reviews = ['Good Condition & Excellent Product ❤️', 'Not as Expected','Actually didn’t expect it to be this good. My main purpose in buying this was to listen to songs and the quality when it comes to listening to songs is really good.']
@@ -16,15 +17,21 @@ def index():
     data['reviews'] = reviews
     data['positive'] = positive
     data['negative'] = negative
+    logging.info('==== Open Home Page ====')
 
     return render_template('index.html', data = data)
 
 @app.route("/", methods = ['post'])
 def my_post():
     text = request.form['text']
+    logging.info(f'Text: {text}')
+
     preprocessed_text = preprocessing(text)
+    logging.info(f'Preprocessed Text: {preprocessed_text}')
     vectorized_text = vectorizer(preprocessed_text)
+    logging.info(f'Vectorized Text: {vectorized_text}')
     prediction = get_prediction(vectorized_text)
+    logging.info(f'Prediction: {prediction}')
 
     if prediction == 'negative':
         global negative
